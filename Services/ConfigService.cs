d3dd036e43ca22a9d6b94a8cf79a9605971d3d9a
Alexander Emelyanov <contact.emelyanov@gmail.com>
2020-07-17 01:43:09 +0300

    Ну блять пиздец

--- Services/ConfigService.cs
+++ Services/ConfigService.cs
@@ -1,10 +1,95 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DisgraceDiscordBot.Entities;
+using Newtonsoft.Json;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DisgraceDiscordBot.Services
 {
-    class ConfigService
+    public class ConfigService
     {
+        public BotConfig BotConfig
+        {
+            get
+            {
+                if (configCache == null)
+                {
+                    configCache = GetConfig();
+                }
+
+                return configCache;
+            }
+            set
+            {
+                configCache = value;
+                SetConfig(value);
+            }
+        }
+        private BotConfig configCache;
+
+        private LogService _logService;
+
+        public ConfigService()
+        {
+            if (!File.Exists("config.json"))
+            {
+                SetConfig(new BotConfig());
+            }
+        }
+
+        public ConfigService (LogService logService)
+        {
+            _logService = logService;
+
+            if (!File.Exists("config.json"))
+            {
+                logService.Log(DSharpPlus.LogLevel.Info, "ConfigService", "Config not exists");
+                SetConfig(new BotConfig());
+            }
+        }
+
+        public async Task<BotConfig> GetConfigAsync()
+        {
+            var json = "";
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync();
+
+            return JsonConvert.DeserializeObject<BotConfig>(json);
+        }
+
+        public async Task SetConfigAsync(BotConfig config)
+        {
+            if (_logService != null)
+                _logService.Log(DSharpPlus.LogLevel.Debug, "ConfigService", "Writing config file async");
+
+            var cfgjson = JsonConvert.SerializeObject(config);
+
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamWriter(fs, new UTF8Encoding(false)))
+                await sr.WriteAsync(cfgjson);
+        }
+
+        public BotConfig GetConfig()
+        {
+            var json = "";
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = sr.ReadToEnd();
+
+            return JsonConvert.DeserializeObject<BotConfig>(json);
+        }
+
+        public void SetConfig(BotConfig config)
+        {
+            if (_logService != null)
+                _logService.Log(DSharpPlus.LogLevel.Debug, "ConfigService", "Writing config file");
+
+            var cfgjson = JsonConvert.SerializeObject(config);
+
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamWriter(fs, new UTF8Encoding(false)))
+                sr.Write(cfgjson);
+        }
     }
 }
---
+++ Services/LogService.cs
@@ -0,0 +1,20 @@
+﻿using DSharpPlus;
+using System;
+
+namespace DisgraceDiscordBot.Services
+{
+    public class LogService
+    {
+        private DiscordClient _client;
+
+        public LogService(DiscordClient client)
+        {
+            _client = client;
+        }
+
+        public void Log(LogLevel level, string application, string message)
+        {
+            _client.DebugLogger.LogMessage(level, application, message, DateTime.Now);
+        }
+    }
+}
