e1d9617b94a285a10cd3239c15d48c7337ff7d7f
Alexander Emelyanov <contact.emelyanov@gmail.com>
2020-07-17 00:45:50 +0300

    Update BotConfig.cs

--- Entities/BotConfig.cs
+++ Entities/BotConfig.cs
@@ -6,12 +6,12 @@
 
         public string CommandPrefix { get; private set; } = "!";
 
-        public int GoodColor { get; set; } = 0xFF0000;
+        public int GoodColor { get; set; } = 0x00FF00;
 
         public int BadColor { get; set; } = 0xFF0000;
 
-        public int TimeoutColor { get; set; } = 0xFF0000;
+        public int TimeoutColor { get; set; } = 0xFFFF00;
 
-        public int CommonColor { get; set; } = 0xFF0000;
+        public int CommonColor { get; set; } = 0x404040;
     }
 }
