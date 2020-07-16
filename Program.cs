8d3029a0c4937b60b1a5258aff1154405b3d7011
Alexander Emelyanov <contact.emelyanov@gmail.com>
2020-07-16 18:22:15 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NationDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
