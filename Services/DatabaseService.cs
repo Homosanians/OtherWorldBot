ded65c808bca093f8fbb99f7fa3b8f41a93cf4fa
Alexander Emelyanov <contact.emelyanov@gmail.com>
2020-07-17 02:39:49 +0300

    Update DatabaseService.cs

--- Services/DatabaseService.cs
+++ Services/DatabaseService.cs
@@ -6,6 +6,10 @@ namespace DisgraceDiscordBot.Services
 {
     class DatabaseService
     {
-
+        // SQLite3 (EF6)
+        // Databse file: bot.db
+        // Database name: DisgraceDiscordBot
+        // Collection name: Counties
+        // Entry name: Country
     }
 }
