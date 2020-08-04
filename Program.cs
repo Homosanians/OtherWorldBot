2c8836d8ff84962625e034739819c3fad15459e1
Alexander Emelyanov <35880220+Homosanians@users.noreply.github.com>
2020-08-04 23:19:30 +0300

    Delete redundant comment

--- Program.cs
+++ Program.cs
@@ -4,8 +4,6 @@
     {
         public static void Main(string[] args)
         {
-            // Since we cannot make the entry method asynchronous,
-            // let's pass the execution to asynchronous code
             new Bot().InitAsync().GetAwaiter().GetResult();
         }
     }
